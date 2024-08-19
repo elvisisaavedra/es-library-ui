27b0414f7e8fcf1f9f9b3b3441b832760f0c1706
Elvis Saavedra <elvisisaavedra@yape.com.pe>
2024-08-19 00:58:03 -0500

    update package and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'lib/main.ts'),
+      name: 'ESReactUI',
+      // the proper extensions will be added
+      fileName: 'es-react-ui',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
+        },
+      },
+    },
+  },
 })
