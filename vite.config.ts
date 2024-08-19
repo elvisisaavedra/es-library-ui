4b87a741a372339ac1b04c50911d38de10ef5a60
Elvis Saavedra <elvisisaavedra@yape.com.pe>
2024-08-19 01:55:37 -0500

    add types

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    dts({
+      tsconfigPath: resolve(__dirname, 'tsconfig.app.json'),
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
